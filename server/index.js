877f93533edcf0149c5765f98f51c6f17d5d6985
Harish S <harishs0102@gmail.com>
2021-11-16 18:02:48 +0530

    initial commit-5

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log("hii");
+require("dotenv").config("");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send({
+    status: "200",
+    message: "Home page working",
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is UP!! in ${process.env.PORT}`);
+});
